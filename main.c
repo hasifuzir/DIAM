a4c67cc07884888eb963744e7c1ef7ea45296e75
imandsy <iman.ds.yahya@gmail.com>
2016-12-14 13:26:21 +0000

    Update main.c

--- main.c
+++ main.c
@@ -152,6 +152,35 @@ int main(void)
             MAP_ADC14_enableSampleTimer(ADC_MANUAL_ITERATION);
         //==--=--==-=-=-=--=
 
+    /* Configuring pins for HFXT crystal */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_PJ,
+            GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN0);
+
+
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0 | GPIO_PIN1 | GPIO_PIN2 | GPIO_PIN3 | GPIO_PIN4 | GPIO_PIN5 | GPIO_PIN6 | GPIO_PIN7);
+
+
+    /* Configuring GPIOs (P4.3 MCLK) */
+       MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P4, GPIO_PIN3,
+       GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Setting the external clock frequency. This API is optional, but will
+     * come in handy if the user ever wants to use the getMCLK/getACLK/etc
+     * functions
+     */
+    CS_setExternalClockSourceFrequency(32000,48000000);
+
+    /* Starting HFXT in non-bypass mode without a timeout. Before we start
+     * we have to change VCORE to 1 to support the 48MHz frequency */
+    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+    CS_startHFXT(false);
+
+    /* Initializing MCLK to HFXT (effectively 48MHz) */
+    MAP_CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
      /* Enabling MASTER interrupts */
     MAP_Interrupt_enableMaster();
